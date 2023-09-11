d6e6dffc10909d5c23cf06f7a0a92f88777b238c
Ruhao Chen <rchen@bayviewglen.ca>
2023-09-11 10:57:56 -0400

    ExampleOne

--- Unit One - Introduction to Java/src/lesson2/ExampleOne.java
+++ Unit One - Introduction to Java/src/lesson2/ExampleOne.java
@@ -3,6 +3,6 @@ package lesson2;
 public class ExampleOne {
     public static void main(String[] args) {
         System.out.println("Hi ICS3U!");
-
+        System.out.println("Goodbye ICS3U!");
     }
 }
