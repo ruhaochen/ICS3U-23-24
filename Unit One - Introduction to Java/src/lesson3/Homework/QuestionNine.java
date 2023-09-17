066e853b1d06a61eb1b8e23759f176b43a49bdb1
Ruhao Chen <rchen@bayviewglen.ca>
2023-09-17 16:45:21 -0400

    Edited question 9

--- Unit One - Introduction to Java/src/lesson3/Homework/QuestionNine.java
+++ Unit One - Introduction to Java/src/lesson3/Homework/QuestionNine.java
@@ -4,9 +4,9 @@ public class QuestionNine {
     public static void main(String[] args) {
         double base = 9.8;
         double height = 5.2; 
-        double area = base*height/2; 
+        double area = (base*height)/2; 
 
         System.out.println("The area of the triangle is " + area);
-        
+
     }
 }
